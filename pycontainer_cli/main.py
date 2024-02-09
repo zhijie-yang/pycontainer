1bac822c0ac5555148f5fba902920382c88fe6cb
zhijie-yang <zhijie.yang@tum.de>
2024-02-09 12:47:25 +0100

    Create python-package-conda.yml

--- pycontainer_cli/main.py
+++ pycontainer_cli/main.py
@@ -1,19 +1,25 @@
 import argparse
-import sys
 import os
+import sys
+
 import pycontainer
 
 
 def parse_args(argv: list[str]):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--volume', action='append', 
-                        help='Volume mapping, /source/path:/target/path')
-    parser.add_argument('-n', '--name', required=True, type=str,
-                        help='Name of the container')
-    parser.add_argument('-r', '--rootfs', required=True, type=str,
-                        help='Path to container image rootfs')
-    parser.add_argument('command', nargs=argparse.REMAINDER, 
-                        help='The command to run')
+    parser.add_argument(
+        "-v",
+        "--volume",
+        action="append",
+        help="Volume mapping, /source/path:/target/path",
+    )
+    parser.add_argument(
+        "-n", "--name", required=True, type=str, help="Name of the container"
+    )
+    parser.add_argument(
+        "-r", "--rootfs", required=True, type=str, help="Path to container image rootfs"
+    )
+    parser.add_argument("command", nargs=argparse.REMAINDER, help="The command to run")
     return parser.parse_args()
 
 
@@ -24,7 +30,7 @@ def main(argv: list[str] = sys.argv):
     args = parse_args(argv)
 
     if args.volume:
-        mapped_paths = [tuple(kv.split(':')) for kv in args.volume]
+        mapped_paths = [tuple(kv.split(":")) for kv in args.volume]
         for kv in mapped_paths:
             assert len(kv) == 2
     else:
@@ -34,5 +40,6 @@ def main(argv: list[str] = sys.argv):
     container = pycontainer.Container(args.name, args.rootfs, mapped_paths)
     container.start(args.command)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
